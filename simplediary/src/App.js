94a8499ba1e1c2ba498e85debf2b0c848d150594
mintropy <jun2korea@gmail.com>
2022-09-02 17:11:57 +0900

    diary list & item using props

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "mintropy",
+    content: "Hello",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "mintropy",
+    content: "Good Bye",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
