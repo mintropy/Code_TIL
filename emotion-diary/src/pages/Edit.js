19a2948e5f9ddeda38691149c4fc984ac1091105
mintropy <jun2korea@gmail.com>
2022-09-07 10:53:50 +0900

    react-router-dom, use parameters

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,8 +1,18 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
   return (
     <div>
       <h1>Edit</h1>
       <p>this is page for Edit one</p>
+      <button onClick={() => navigate("/home")}>home</button>
+      <button onClick={() => navigate(-1)}>back</button>
     </div>
   );
 };
