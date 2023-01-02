d462ba74bd8442000a5a390838f1eb30110523b2
mintropy <jun2korea@gmail.com>
2023-01-02 09:38:30 +0900

    Java sample class

---
+++ Lectures/udemy - Complete Java for Automation/CoreJavaTraining/FirstClass.java
@@ -0,0 +1,11 @@
+package CoreJavaTraining;
+
+public class FirstClass {
+
+    static int a = 4;
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!!");
+        System.out.println(a);
+    }
+}
---
+++ Lectures/udemy - Complete Java for Automation/README.md
@@ -0,0 +1,2 @@
+# udemy - Complete Java for Automation
+- link : https://www.udemy.com/course/java-code-made-easy/
