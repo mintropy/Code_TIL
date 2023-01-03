85cde3a4836cc328f13e723c3423d2037614cabe
mintropy <jun2korea@gmail.com>
2023-01-03 09:43:56 +0900

    FastAPI query parameter

--- FastAPI/FastAPI Tutorial/main.py
+++ FastAPI/FastAPI Tutorial/main.py
@@ -21,6 +21,11 @@ async def get_item(item_id: int):
     return {"item_id": item_id}
 
 
+@app.get("/item/")
+async def get_item_detail(start: int = 0, end: int = 0):
+    return {"count": end - start + 1}
+
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
