914b6406015da21c4dfbe250e25d00eaa53e299e
mintropy <jun2korea@gmail.com>
2023-01-05 09:16:23 +0900

    Java using method

--- Lectures/udemy - Complete Java for Automation/CoreJavaTraining/FirstClass.java
+++ Lectures/udemy - Complete Java for Automation/CoreJavaTraining/FirstClass.java
@@ -1,14 +1,24 @@
 package CoreJavaTraining;
 
 public class FirstClass {
-    public String getData() {
+    int b = 5;
+
+    // Method
+    public void getData() {
         // static int a = 4;
         System.out.println("I am in method");
-        return "Hello";
+        // return "Hello";
     }
 
     public static void main(String[] args) {
+        FirstClass fn = new FirstClass();
+        fn.getData();
+        System.out.println(fn.b);
+
+        SecondClass sn = new SecondClass();
+        sn.setData();
+
         System.out.println("Hello Java!!");
-        System.out.println(a);
+        // System.out.println(a);
     }
 }
---
+++ Lectures/udemy - Complete Java for Automation/CoreJavaTraining/SecondClass.java
@@ -0,0 +1,7 @@
+package CoreJavaTraining;
+
+public class SecondClass {
+    public void setData() {
+        System.out.println("I am in second class");
+    }
+}
