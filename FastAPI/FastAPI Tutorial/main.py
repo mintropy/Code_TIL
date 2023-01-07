2ba096445f3ed3b6709eb2ef6982fc82ad704080
mintropy <jun2korea@gmail.com>
2023-01-07 19:34:33 +0900

    FastAPI body WIP

--- FastAPI/FastAPI Tutorial/main.py
+++ FastAPI/FastAPI Tutorial/main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, Body
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -24,33 +24,17 @@ async def root():
     return {"message": "Hello, FastAPI"}
 
 
-@app.get("/items/{item_id}")
+@app.post("/items/{item_id}")
 async def get_item(
     *,
     item_id: int = Path(title="The ID of item to get", ge=0, le=10000),
     q: str | None = Query(default=None, max_length=50),
-    size: float = Query(gt=0, lt=10.5)
+    item: Item | None = None,
+    importance: int = Body()
 ):
-    return {"item_id": item_id}
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.price:
-        item_dict["price"] = int(item_dict["price"] * (10 / 9))
-    return {**item_dict}
-
-
-@app.get("/item/")
-async def get_item_detail(start: int = 0, end: int = 0):
-    return {"count": end - start + 1}
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "Have some residuals"}
+    return {
+        "item_id": item_id,
+        "q": q,
+        "item": item,
+        "importance": importance,
+    }
