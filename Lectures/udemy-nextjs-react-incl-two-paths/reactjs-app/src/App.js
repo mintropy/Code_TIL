17294ab79d07e0cdb89ac5a462d12458e800f1a7
mintropy <jun2korea@gmail.com>
2023-06-21 22:05:32 +0900

    simple card item

--- Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app/src/App.js
+++ Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app/src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Todos</h1>
+      <div className="card">
+        <h2>title</h2>
+        <div className="actions">
+          <button className="btn">DELETE</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
