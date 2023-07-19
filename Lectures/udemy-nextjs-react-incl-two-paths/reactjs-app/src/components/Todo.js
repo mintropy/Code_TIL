85da13b571bde033a2f664d00f4ea055b28a0162
mintropy <jun2korea@gmail.com>
2023-07-19 22:19:54 +0900

    react on click event

--- Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app/src/components/Todo.js
+++ Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app/src/components/Todo.js
@@ -1,9 +1,15 @@
 function Todo(props) {
+  function deleteHandler() {
+    
+  }
+
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <div className="actions">
-        <button className="btn">DELETE</button>
+        <button className="btn" onClick={deleteHandler}>
+          DELETE
+        </button>
       </div>
     </div>
   );
