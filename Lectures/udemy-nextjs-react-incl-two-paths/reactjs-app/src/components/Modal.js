f4bb88a880469b85d4c03acd77abcad33ad4321e
mintropy <jun2korea@gmail.com>
2023-07-23 09:42:53 +0900

    react components

---
+++ Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app/src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+  return (
+    <div classname="modal">
+      <p>Are You Sure?</p>
+      <button classname="btn btn--alt">Cancle</button>
+      <button classname="btn">Confirm</button>
+    </div>
+  );
+}
+
+export default Modal;
