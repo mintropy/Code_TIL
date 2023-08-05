f3dffe70ff7ceb8aedb122198396677f43d369cd
mintropy <jun2korea@gmail.com>
2023-08-05 23:02:43 +0900

    react - send form

--- Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app-2/src/components/meetups/NewMeetUpForm.js
+++ Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app-2/src/components/meetups/NewMeetUpForm.js
@@ -1,30 +1,60 @@
+import { useRef } from "react";
+
 import Card from "../ui/Card";
 import styles from "./NewMeetUpForm.module.css";
 
 function NewMeetUpForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetUpData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    console.log(meetUpData);
+  }
+
   return (
     <Card>
-      <from className={styles.form}>
+      <form className={styles.form} onSubmit={submitHandler}>
         <div className={styles.control}>
           <label htmlFor="title">MeetUp Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="image">MeetUp Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputRef}
+          ></textarea>
         </div>
         <div className={styles.actions}>
           <button>Add MeetUp</button>
         </div>
-      </from>
+      </form>
     </Card>
   );
 }
