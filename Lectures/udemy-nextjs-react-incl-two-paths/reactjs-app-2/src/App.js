977c7862de5875a4c6330565ee257c4a6fe3051b
mintropy <jun2korea@gmail.com>
2023-08-05 22:39:38 +0900

    react wrapper component

--- Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app-2/src/App.js
+++ Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app-2/src/App.js
@@ -4,22 +4,24 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
 import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupPage />
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
