79b130319237ad5a4074007fe8904b41ae0ecacb
mintropy <jun2korea@gmail.com>
2023-08-06 14:23:08 +0900

    edit module name

--- Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app-2/src/components/layout/MainNavigation.js
+++ Lectures/udemy-nextjs-react-incl-two-paths/reactjs-app-2/src/components/layout/MainNavigation.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import classes from "./MainNaviation.module.css";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
