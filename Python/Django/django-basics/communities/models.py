97ae41c611339815a44bef41f7abc01b05b0c2ce
Youngjun Lee <jun2korea@gmail.com>
2024-02-18 22:28:24 +0900

    django base model design

--- Python/Django/django-basics/communities/models.py
+++ Python/Django/django-basics/communities/models.py
@@ -1,10 +1,15 @@
-import uuid
-
 from django.db import models
 
-from config.models import BaseModel
+from modules.models import BaseModel
 
 
 class Community(BaseModel):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
+
+
+class Post(BaseModel):
+    community = models.ForeignKey(
+        Community, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
--- Python/Django/django-basics/config/settings.py
+++ Python/Django/django-basics/config/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "modules",
     "communities",
     "rest_framework",
     "silk",
