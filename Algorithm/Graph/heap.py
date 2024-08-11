ffea2c5f1758fe6d662de90c85d34ad2adb3e375
Youngjun Lee <jun2korea@gmail.com>
2024-08-11 10:17:45 +0900

    :sparkles: add heap test code, support same value

--- Algorithm/Graph/heap.py
+++ Algorithm/Graph/heap.py
@@ -1,17 +1,26 @@
 from operator import gt, lt
+from random import randint
 
 
 class Heap:
     def __init__(self, max_heap: bool = True) -> None:
         self._heap: list[int] = [0]
         self.max_heap = max_heap
-        self._operation = lt if max_heap else gt
+        self._operation = gt if max_heap else lt
+
+    def __len__(self) -> int:
+        return len(self._heap) - 1
+
+    def compare(self, child_idx: int) -> bool:
+        return self._heap[child_idx] == self._heap[child_idx // 2] or self._operation(
+            self._heap[child_idx // 2], self._heap[child_idx]
+        )
 
     def _push_heapify(self) -> None:
         idx = len(self._heap) - 1
         while idx > 1:
             parent_idx = idx // 2
-            if self._operation(self._heap[parent_idx], self._heap[idx]):
+            if not self.compare(idx):
                 self._heap[parent_idx], self._heap[idx] = (
                     self._heap[idx],
                     self._heap[parent_idx],
@@ -36,7 +45,7 @@ class Heap:
             else:
                 child_idx = right_child_idx
                 left_child = False
-            if self._operation(self._heap[idx], self._heap[child_idx]):
+            if not self.compare(child_idx):
                 self._heap[idx], self._heap[child_idx] = (
                     self._heap[child_idx],
                     self._heap[idx],
@@ -59,6 +68,39 @@ class Heap:
         return num
 
 
+def verify_heap(heap: Heap) -> bool:
+    heap_len = len(heap)
+    if not heap_len:
+        return True
+
+    last_idx = heap_len + 1
+
+    for i in range(1, last_idx):
+        left, right = i * 2, i * 2 + 1
+        try:
+            if not heap.compare(left):
+                return False
+            if not heap.compare(right):
+                return False
+        except IndexError:
+            pass
+    return True
+
+
+def test_heap() -> None:
+    heap = Heap()
+
+    for _ in range(10_000):
+        heap.push(randint(1, 100_000))
+        if not verify_heap(heap):
+            raise Exception("Validation not passed")
+    for _ in range(10_000):
+        heap.pop()
+        verify_heap(heap)
+        if not verify_heap(heap):
+            raise Exception("Validation not passed")
+
+
 if __name__ == "__main__":
     heap = Heap()
 
@@ -69,3 +111,5 @@ if __name__ == "__main__":
     for _ in range(6):
         print(heap.pop())
         print(heap._heap)
+
+    test_heap()
