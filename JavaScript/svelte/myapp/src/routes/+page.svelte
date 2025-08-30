70857ecd0b6f23d011740a293c3d06c8e622544c
Youngjun Lee <jun2korea@gmail.com>
2025-08-30 09:41:24 +0900

    svelte sample app

---
+++ JavaScript/svelte/myapp/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    console.log("Page mounted");
+  });
+
+  let name: string = "SvelteKit";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+<p>
+  Hello, {name}! This is your first SvelteKit page.
+</p>
